2e1ff2f841caa661db9854af409cb884200a256e
Okonu <ianyakundi015@gmail.com>
2023-04-01 23:12:00 +0300

    add mux, comments, render, update

--- main.go
+++ main.go
@@ -13,17 +13,44 @@ type Welcome struct {
 }
 
 func main() {
+	// Set up the welcome message with the current time
 	welcome := Welcome{"Sale Begins Now", time.Now().Format(time.Stamp)}
+
+	// Load the HTML template file
 	template := template.Must(template.ParseFiles("template/template.html"))
 
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if sale := r.FormValue("sale"); sale != "" {
-			welcome.Sale = sale
+	// Set up the HTTP server
+	mux := http.NewServeMux()
+
+	// Serve static files from the "static" directory
+	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	// Handle the root URL
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Check if the request method is POST
+		if r.Method == http.MethodPost {
+			// Update the welcome message with the new sale message from the form
+			r.ParseForm()
+			if sale := r.Form.Get("sale"); sale != "" {
+				welcome.Sale = sale
+			}
 		}
+
+		// Render the HTML template with the welcome message
 		if err := template.ExecuteTemplate(w, "template.html", welcome); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
-	fmt.Println(http.ListenAndServe(":8000", nil))
+
+	// Start the HTTP server
+	server := &http.Server{
+		Addr:         ":8000",
+		Handler:      mux,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+	fmt.Println("Server started at http://localhost:8000")
+	if err := server.ListenAndServe(); err != nil {
+		panic(err)
+	}
 }
